34af9393e343d5639a698c0a36bb7c589c5cadec
ous81 <54725209+ous81@users.noreply.github.com>
2023-06-13 07:26:36 +0700

    Shooting fire balls

---
+++ Fire.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+   public float speed;
+   private Rigidbody2D rb;
+
+   void Start()
+   {
+    rb = GetComponent<Rigidbody2D>();
+    rb.velocity = transform.right * speed;
+   }
+
+   
+}
